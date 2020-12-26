d575d5069e16bdf7fccbedb6b73cfb6215d2011d
SudhanshuJoshi09 <sudhanshujoshi49@gmail.com>
2020-12-26 22:18:27 +0530

    adding views

--- app/article/serializers.py
+++ app/article/serializers.py
@@ -3,35 +3,14 @@ from article.models import Tags
 from rest_framework import serializers
 
 
-class TagSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    tag_name = serializers.CharField(max_length=10)
-    tag_description = serializers.CharField(max_length=100)
-
-    def create(self, validated_data):
-        """Create a new `tags` from the given validated_data"""
-        return Tags.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """Update a given to the data provided"""
-        instance.tag_name = validated_data.get('tag_name', instance.tag_name)
-        instance.tag_description = validated_data.get('tag_description', instance.tag_description)
-        instance.save()
-        return instance
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tags
+        fields = ['id', 'tag_name', 'tag_description']
 
 
 class ArticleSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=255)
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'description']
     
-    def create(self, validated_data):
-        """Create a new `article` from the given validated_data"""
-        return Article.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """Update a article to the data provided"""
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.save()
-        return instance
--- app/article/views.py
+++ app/article/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from article.models import Tags, Article
+from article.serializers import TagSerializer, ArticleSerializer
 
 
+@csrf_exempt
+def article_list(request):
+    """List all the articles, or create a new article"""
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        if serializer.is_valid():
+            return JsonResponse(serializer.data, status=201)
+        return Jsonresponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def article_detail(request, primary_key):
+    """Retrieve, update or delete a article"""
+    try:
+        article = Article.objects.get(primary_key)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer()
